36dcbbd1e490db943a9f73dd18f3be4dded51abc
pikapikapow <vladimirhwang84@gmail.com>
2024-02-20 10:10:46 -0500

    Finished the project!

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -4,8 +4,16 @@ let i;
 
 for (i = 0; i < questions.length; i++) {
   questions[i].addEventListener("click", function () {
-    // this.classList.toggle("open");
-
+    let answerArr = document.getElementsByClassName("answer");
+    for (let j = 0; j < answerArr.length; j++) {
+      if (answerArr[j].style.display === "block") {
+        answerArr[j].style.display = "none";
+        let questionEl = answerArr[j].previousElementSibling;
+        // console.log(questionEl);
+        questionEl.getElementsByClassName("plus")[0].src =
+          "./assets/images/icon-plus.svg";
+      }
+    }
     let answer = this.nextElementSibling;
     if (answer.style.display === "block") {
       answer.style.display = "none";
